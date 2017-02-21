cecc137602fa6270a7e44bd3c081d8f51a391a45
Mary Ellen Weber <mmweber2@gmail.com>
2017-02-21 17:06:18 -0600

    Read both files and split text of both into lists.

--- artPrompt.js
+++ artPrompt.js
@@ -1,29 +1,23 @@
 // Choose from list of species
 // Choose from list of adjectives
 // Choose from list of verbs (optional)
-// Allow customizing of species?
+// Allow customizing of species? (Categorize species, no fish, etc)
 //
-// TODO: Learn how to import a list of words from a file and parse it
+// TODO: Pick random item from both pools
 //
-var animals_list;
 var adj_list;
 
-function readTextFile() {
-  var requestor = new XMLHttpRequest();
-  requestor.addEventListener("load", function() {
-    console.log(this.responseText);
-  });
-  requestor.open("GET", "/toz/js/animals_list.txt");
-  requestor.send();
-  console.log("Ran readTextFile()");
-}
-
+// Nest both jQuery calls because we need access to both data groups
 jQuery.get('/toz/js/animals_list.txt', function(data) {
-  animals_list = data;
+  var animals_list = data.split("\n");
+  //for (var line = 0; line < animals_list.length; line++) {
+  jQuery.get('/toz/js/adj_full.txt', function(adjs) {
+    adj_list = adjs.split("\n");
+    for (var line = 0; line < 5; line++) {
+      console.log(adj_list[line]);
+    }
+    for (var line = 0; line < 5; line++) {
+      console.log(animals_list[line]);
+    }
+  });
 });
-jQuery.get('/toz/js/adj_full.txt', function(data) {
-  adj_list = data;
-});
-console.log("Loaded the JS.");
-console.log(animals_list);
-
