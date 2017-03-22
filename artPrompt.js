619a7f34404a0e1184d277c13ccc5c97c9d92a54
Mary Ellen Weber <mmweber2@gmail.com>
2017-03-22 17:26:58 -0500

    Change randomize to only reload the prompt.
    Move the list of animals and adjectives into global scope.
    Move the dictionary URL format into the global scope.
    Move getPrompt (for initial or subsequent calls) into its own function.
    Fix an error in getRandom where randInt was not declared with var.

--- artPrompt.js
+++ artPrompt.js
@@ -3,21 +3,34 @@
 // Choose from list of verbs (optional)
 // Allow customizing of species? (Categorize species, no fish, etc)
 //
-// TODO: Pick random item from both pools
-//
-var adj_list;
+//Set once and just choose another random option from the same list each randomization
+var animalsList;
+var adjList;
+var DICT_URL = "<a href=\"http://www.freedictionary.org/?Query=";
 
+function pickRandom(list) {
+  var randInt = Math.random();
+  randInt *= list.length;
+  return list[Math.floor(randInt)];
+}
+
+function getPrompt() {
+  var adj_result = pickRandom(adjList);
+  var adj_link = DICT_URL + "" + adj_result + "\" target=\"_blank\">" + adj_result + "</a>";
+  var animal = pickRandom(animalsList).split("|");
+  var animal_link = "<a href=\"" + animal[1] + "\" target=\"_blank\">" + animal[0] + "</a>";
+  var full_result = adj_link + " " + animal_link;
+  document.getElementById("animal_result").innerHTML = animal_link;
+  document.getElementById("adj_result").innerHTML = adj_link;
+  document.getElementById("full_result").innerHTML = full_result;
+}
+
+// Only call once per page load
 // Nest both jQuery calls because we need access to both data groups
 jQuery.get('/toz/js/animals_list.txt', function(data) {
-  var animals_list = data.split("\n");
-  //for (var line = 0; line < animals_list.length; line++) {
+  animalsList = data.split("\n");
   jQuery.get('/toz/js/adj_full.txt', function(adjs) {
-    adj_list = adjs.split("\n");
-    for (var line = 0; line < 5; line++) {
-      console.log(adj_list[line]);
-    }
-    for (var line = 0; line < 5; line++) {
-      console.log(animals_list[line]);
-    }
+    adjList = adjs.split("\n");
+    getPrompt();
   });
 });
